afd4668c8635683f9ab8b566ee7d6ae49ab7d7bc
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2023-01-19 11:45:47 +0330

    make Connection final in ApplicationContext

--- src/main/java/sa/aref/util/ApplicationContext.java
+++ src/main/java/sa/aref/util/ApplicationContext.java
@@ -3,7 +3,7 @@ package sa.aref.util;
 import java.sql.Connection;
 
 public class ApplicationContext {
-    private static Connection connection = new DBConnection().connect();
+    private static final Connection connection = new DBConnection().connect();
 
     public static Connection getConnection() {
         return connection;
